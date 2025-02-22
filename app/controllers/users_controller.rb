2fc976f3b70259147a38bee748f51c37e4a351cc
barsea <92622141+barsea@users.noreply.github.com>
2025-02-22 18:25:05 +0900

    ユーザー情報表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
+      binding.pry
       redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
